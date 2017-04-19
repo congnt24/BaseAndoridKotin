d5d214f4b41c57eca91dcb9ef0be3237bcff474d
Nguyen Cong <cong.t.nguyen@vntrip.vn>
2017-04-19 17:45:28 +0700

    add disposable to BasePresenter

--- kotlinmvp/src/main/java/com/congnt/kotlinmvp/mvp/presenter/BasePresenter.kt
+++ kotlinmvp/src/main/java/com/congnt/kotlinmvp/mvp/presenter/BasePresenter.kt
@@ -1,6 +1,8 @@
 package com.congnt.kotlinmvp.mvp.presenter
 
 import com.congnt.kotlinmvp.mvp.view.MvpView
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 
 /**
@@ -8,8 +10,25 @@ import com.congnt.kotlinmvp.mvp.view.MvpView
  */
 
 abstract class BasePresenter<V : MvpView> {
+    val disposables: CompositeDisposable by lazy {
+        CompositeDisposable()
+    }
+
     var view: V? = null
     fun attach(view: V) {
         this.view = view
+    }
+
+    fun release() {
+        disposables.clear()
+        view = null
+    }
+
+    protected fun addSubscription(subscription: Disposable) {
+        disposables.add(subscription)
+    }
+
+    protected fun clearSubscriptions() {
+        disposables.clear()
     }
 }
